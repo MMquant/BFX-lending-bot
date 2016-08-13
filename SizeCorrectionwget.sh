72f4fec97fce27f01b4324a7306316b94907d743
Petr Javorik <MMquant@users.noreply.github.com>
2016-08-13 22:19:19 +0200

    Update SizeCorrectionwget.sh

--- SizeCorrectionwget.sh
+++ SizeCorrectionwget.sh
@@ -3,13 +3,15 @@
 # Data file size correction for wget lastSwapsUSD.csv download.
 # Wget tends to download just part of the whole file.
 # We will resume downloads until we get the whole file.
+# Set dlpath before use.
 
 dlsize=0
+dlpath=your/custom/path/lastSwaps.csv
 truesize=$(curl -sI https://bfxdata.com/csv/lastSwapsUSD.csv | grep -i content-length | awk '{print $2}' | tr -d '\r')
 
 while [[ $dlsize != $truesize ]]; do
-/usr/bin/wget -t 0 https://bfxdata.com/csv/lastSwapsUSD.csv --random-wait -O /home/maple/Documents/MATLAB/data/lastSwapsUSD.csv -a /home/maple/Documents/MATLAB/logs/wget.log
-dlsize=$(stat -c%s "/home/maple/Documents/MATLAB/data/lastSwapsUSD.csv")
+/usr/bin/wget -t 0 https://bfxdata.com/csv/lastSwapsUSD.csv --random-wait -O $dlpath -a /home/maple/Documents/MATLAB/logs/wget.log
+dlsize=$(stat -c%s "$dlpath")
 truesize=$(curl -sI https://bfxdata.com/csv/lastSwapsUSD.csv | grep -i content-length | awk '{print $2}' | tr -d '\r')
 # echo "dlsize="$dlsize
 # echo "truesize="$dlsize
